e6651d35360c6c0704a2be981355c87c2314d54c
drownedout <danielcdonnelly@gmail.com>
2018-04-01 14:38:10 -0400

    Improved query efficiencies on query one and two, refactored code

--- main.py
+++ main.py
@@ -1,54 +1,74 @@
 #! /usr/bin/env python3
 import psycopg2
 
-db = psycopg2.connect("dbname=news")
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except BaseException:
+        print("Database did not connect properly")
 
-cursor = db.cursor()
 
-f = open('output.txt', 'w')
+def query_one():
+    db, cursor = connect()
 
-f.write("What are the most popular three articles of all time?" + "\n" * 2)
+    query = "SELECT title, " \
+            "count(*) AS views " \
+            "FROM log join articles ON log.path = concat('/article/', articles.slug) " \
+            "GROUP BY title ORDER BY views DESC LIMIT 3;"
 
-cursor.execute("SELECT articles.title, "
-               "count(*) AS num "
-               "FROM articles "
-               "JOIN log ON log.path "
-               "LIKE '%' || articles.slug "
-               "GROUP BY articles.title "
-               "ORDER BY num "
-               "DESC LIMIT 3;")
+    cursor.execute(query)
 
-for row in cursor:
-    f.write("\"" + str(row[0]) + "\"" + " - " + str(row[1]) + " views" + '\n')
+    for row in cursor:
+        f.write("\"" + str(row[0]) + "\"" + " - " +
+                str(row[1]) + " views" + '\n')
 
-f.write("\n" * 2)
+    db.close()
 
-f.write("Who are the most popular article authors of all time?" + "\n" * 2)
 
-cursor.execute("SELECT authors.name, "
-               "count(*) AS num "
-               "FROM authors "
-               "INNER JOIN articles ON articles.author = authors.id "
-               "INNER JOIN log on log.path LIKE '%' || articles.slug "
-               "GROUP BY authors.name ORDER BY num DESC;")
+def query_two():
+    db, cursor = connect()
 
-for row in cursor:
-    f.write(str(row[0]) + " - " + str(row[1]) + " views" + '\n')
+    query = "SELECT authors.name, " \
+            "count(*) AS num FROM authors " \
+            "INNER JOIN articles ON articles.author = authors.id " \
+            "INNER JOIN log ON log.path = concat('/article/', articles.slug) " \
+            "GROUP BY authors.name ORDER BY num DESC;"
 
-f.write("\n" * 2)
+    cursor.execute(query)
 
-f.write("On which days did more than 1% of "
-        "requests lead to errors?" + ("\n" * 2))
+    for row in cursor:
+        f.write(str(row[0]) + " - " + str(row[1]) + " views" + '\n')
 
-cursor.execute(
-    "SELECT * FROM (SELECT date_trunc('day',log.time) as d, "
-    "COUNT(*) FILTER (WHERE status = '404 NOT FOUND')::NUMERIC * 100"
-    "/ COUNT(*) as var "
-    "FROM log GROUP BY d) "
-    "AS fof WHERE fof.var > 1.0;")
+    db.close()
 
-for row in cursor:
-    f.write(str(row[0].strftime("%B %d %Y")) + " - " +
-            str(round(row[1], 2)) + "% errors" + '\n')
 
-f.close()
+def query_three():
+    db, cursor = connect()
+
+    query = "SELECT * FROM (SELECT date_trunc('day',log.time) as d, " \
+            "COUNT(*) FILTER (WHERE status = '404 NOT FOUND')::NUMERIC * 100 / COUNT(*) as var " \
+            "FROM log GROUP BY d) AS fof WHERE fof.var > 1.0;"
+
+    cursor.execute(query)
+
+    for row in cursor:
+        f.write(str(row[0].strftime("%B %d %Y")) + " - " +
+                str(round(row[1], 2)) + "% errors" + '\n')
+
+    db.close()
+
+
+if __name__ == '__main__':
+    f = open('output.txt', 'w')
+    f.write("What are the most popular three articles of all time?" + "\n" * 2)
+    query_one()
+    f.write("\n" * 2)
+    f.write("Who are the most popular article authors of all time?" + "\n" * 2)
+    query_two()
+    f.write("\n" * 2)
+    f.write("On which days did more than 1% of "
+            "requests lead to errors?" + ("\n" * 2))
+    query_three()
+    f.close()
